97961c86239c4f3057f89c2cc05d06df6ca66df0
yannafaith <yannafaith97@gmail.com>
2019-01-23 15:53:03 -0700

    got data loaded onto state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  
+  componentDidMount = () => {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(res => this.setState({friends: res.data}))
+    .catch(err => console.log(err))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Friends</h1>
       </div>
     );
   }
